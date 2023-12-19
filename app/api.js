615eb5d3d59c453c3e7e6bcc28f78de418f5853a
Ishak Benfredj <ishakbanfradje@gmail.com>
2023-12-19 22:56:03 +0100

    chnages

--- app/api.js
+++ app/api.js
@@ -3,6 +3,8 @@ const axios = require("axios");
 const Axios = axios.create({
   baseURL: 'https://portfolio-server-uh48.onrender.com'
   // baseURL: "http://localhost:4000",
+    // baseURL: "http://192.168.43.36:4000",
+
 });
 
 module.exports = Axios;
--- app/dashboard/messages/page.jsx
+++ app/dashboard/messages/page.jsx
@@ -1,6 +1,7 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
 import Axios from "../../api";
+import Link from "next/link";
 
 export default function Messages() {
   const [messages, setMessages] = useState([]);
@@ -14,9 +15,27 @@ export default function Messages() {
     getMessages();
   }, []);
 
-  return <div>
-    {
-        messages.map(m => (<p>{m.message}</p>))
-    }
-  </div>;
+  return (
+    <div className="md:p-0 pt-10">
+      {messages.map((m,index) => (
+        <div className={`p-3 ${index %2 !== 0 && 'bg-gray-900'}`}>
+          <div className="flex gap-3">
+            <h3 className="font-bold text-white capitalize">{m.name}</h3>
+            {m.isWork && (
+              <span class="relative flex h-3 w-3">
+                <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                <span class="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+              </span>
+            )}
+          </div>
+          <div className="flex items-center gap-2 text-gray-300 text-sm">
+              <Link href={`mailto:${m.email}`}>{m.email}</Link>
+              <span className="w-1 h-1 rounded-full bg-gray-200"></span>
+              <Link href={`tel:${m.phone}`}>{m.phone}</Link>
+          </div>
+          <p className="text-white md:w-10/12 mt-3">{m.message}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
