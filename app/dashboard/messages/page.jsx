5cc6dceddc0fb3f83aadeb57e252a8bd26a68730
Ishak Benfredj <ishakbanfradje@gmail.com>
2023-12-19 23:18:39 +0100

    changes

--- app/dashboard/messages/page.jsx
+++ app/dashboard/messages/page.jsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import Axios from "../../api";
 import Link from "next/link";
+import { AiFillDelete, AiFillEye } from "react-icons/ai";
 
 export default function Messages() {
   const [messages, setMessages] = useState([]);
@@ -9,6 +10,19 @@ export default function Messages() {
   const getMessages = async () => {
     const { data } = await Axios.get("/messages");
     setMessages(data);
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      const confirmDeleting = confirm("Confirm Deleting ?");
+      if (confirmDeleting) {
+        await Axios.delete(`/messages/delete/${id}`);
+        getMessages()
+        alert("Deleting Success");
+      }
+    } catch (error) {
+      alert("Something Wrong");
+    }
   };
 
   useEffect(() => {
@@ -33,7 +47,13 @@ export default function Messages() {
               <span className="w-1 h-1 rounded-full bg-gray-200"></span>
               <Link href={`tel:${m.phone}`}>{m.phone}</Link>
           </div>
-          <p className="text-white md:w-10/12 mt-3">{m.message}</p>
+          <div className="flex md:items-center md:flex-row flex-col gap-3">
+            <p className="text-white md:w-10/12 mt-3">{m.message}</p>
+            <div className="flexCenter gap-4 md:w-2/12">
+              <AiFillEye size={30} className=" text-white p-1 bg-secondary rounded-lg cursor-pointer" />
+              <AiFillDelete size={30} className=" text-white p-1 bg-red-700 rounded-lg cursor-pointer" onClick={()=>handleDelete(m._id)} />
+            </div>
+          </div>
         </div>
       ))}
     </div>
